38e5bb81afda8c28ad59bb2bb33a866ccff4c27e
Gregory Connelly <goconnelly@mines.edu>
2017-03-15 22:37:32 -0600

    Changed setConfig Files and things

--- src/tests/gameSetupTests.java
+++ src/tests/gameSetupTests.java
@@ -5,10 +5,18 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import clueGame.Board;
+
 public class gameSetupTests {
+	private static Board board;
+
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		board = Board.getInstance();
+		// Sets the config files to our layout and legend files
+		board.setConfigFiles("JOMW_ClueLayout.csv", "JOMW_ClueLegend.txt", "MKGCv2_PlayersFile.txt"); 
+		board.initialize();
 	}
 
 	// Tests for loading the people
