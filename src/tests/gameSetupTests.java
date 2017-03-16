8a3506d8a88ea349e595eceaec5adbb86a0bfbd3
Megan Kallis <mkallis@mines.edu>
2017-03-15 21:52:08 -0600

    Created gameSetupTests class

---
+++ src/tests/gameSetupTests.java
@@ -0,0 +1,31 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class gameSetupTests {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	// Tests for loading the people
+	@Test
+	public void testPeople() {
+		fail("Not yet implemented");
+	}
+	
+	// Tests for loading/creating the deck of cards
+	@Test
+	public void testLoadingCards() {
+		fail("Not yet implemented");
+	}
+	
+	// Tests for dealing the cards
+	@Test
+	public void testDealingCards() {
+		fail("Not yet implemented");
+	}
+}
