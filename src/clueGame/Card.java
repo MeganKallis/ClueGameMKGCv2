65b3d3c24827ccce37f7adaa45514aa3da446dea
Gregory Connelly <goconnelly@mines.edu>
2017-03-15 21:57:28 -0600

    created stubs for Player and Card Classes

---
+++ src/clueGame/Card.java
@@ -0,0 +1,8 @@
+package clueGame;
+
+public class Card {
+	private String cardName;
+	public boolean equals(){
+		return true;
+	}
+}
--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -1,5 +1,16 @@
 package clueGame;
 
-public class Player {
+import java.awt.Color;
 
+public class Player {
+	private String playerName;
+	private int row;
+	private int col;
+	public Color color;
+	
+	public Card disproveSuggestion(Solotion suggestion){
+		Card temp = new Card();
+		return temp;
+	}
+	
 }
